d52c998e252e1b4a144d8ecd608a8569ba966a51
adison_han <adison330@163.com>
2018-07-02 18:05:14 +0800

    ceshi

--- test1.py
+++ test1.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python
-import matplotlib.pyplot as plt
-
-squares = [1,4,9,16,15]
-plt.plot(squares)
-plt.show()
-
 def testFun():
     temp = [lambda x : i*x for i in range(4)]
     return temp
