3f6d5dcd91124991869c5c014377456340e8a17e
adison_han <adison330@163.com>
2018-07-05 11:29:16 +0800

    test

---
+++ test011.py
@@ -0,0 +1,15 @@
+#! usr/bin/env python
+i = 0
+
+while i < 10:
+
+    #当i==7的时候不希望执行相应代码
+    if i == 7:
+        #在用continue之前，一定要修改计数器
+        i += 1
+
+        continue
+
+    print (i)
+
+    i += 1
