d77cc90005f9ee70440765d0e59e543c876bafa7
adison_han <adison330@163.com>
2018-07-05 11:29:26 +0800

    test

---
+++ test008.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# 定义一个布尔型变量 is_employee,编写代码代码判断是否是本公司员工
+is_employee = True
+
+if not is_employee:
+    print ("非请勿进")
---
+++ test010.py
@@ -0,0 +1,23 @@
+#! usr/bin/env python
+name = '小明'
+
+#解释器知道这里定义了一个函数
+
+def say_hello():
+    print("hello 1")
+    print("hello 2")
+    print("hello 3")
+
+print(name)
+
+say_hello
+
+print(name)
+
+def sum_2_num(num1,num2):
+
+    result = num1 + num2
+
+    print("%d + %d = %d" % (num1,num2,result))
+
+sum_2_num(30,20)
