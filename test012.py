3a98bacb3088c4fa31678ea90d0655943162ccc3
adison_han <adison330@163.com>
2018-07-06 17:28:38 +0800

    test

---
+++ test012.py
@@ -0,0 +1,19 @@
+#! usr/bin/env python
+fram imageai.Detection import ObjectDetection
+import OS
+
+execution_path = os.getcwd()
+
+detector = ObjectDetection()
+detector.setModelTypeAsRetinNet()
+detector.setModelPath( os.path.join(execution_path , "resent50_coco_best_v2.0.1.h5"))
+detector.loadModel()
+custom_objects = detector.CustomObjects(person = True car = False)
+detections = detector.detectCustomObjectsFromImage(input_image=os.path.join(execution_path , "image.png"),
+                                                   output_image_path=os.path.join(execution_path , "image_new.png"),
+                                                   custom_objects=custom_objects , minimum_percentage_probability=65)
+
+for eachObject in detections:
+    print(eachObject["name"] + ":" eachObject["percentage_probability"])
+    print("-" * 50)
+
---
+++ test013.py
@@ -0,0 +1,15 @@
+#! usr/bin/env python
+num_str = "1234567890"
+
+print(num_str[2:6])
+
+print(num_str[2:])
+
+print(num_str[:6])
+
+print(num_str[ : ])
+
+print(num_str[ : : 2])
+
+print(num_str[1: : 2])
+
