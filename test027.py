8216f768ade3c95f3a8e638e6abb1691e8a11833
adison_han <[38833263@qq.com]>
2018-07-17 16:34:23 +0800

    修改能不能提交上去？

---
+++ test027.py
@@ -0,0 +1,14 @@
+#! usr/bin/env python
+
+class Car():
+    def _init_(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def get_descriptive_name(self):
+        long_name = str(self.year) + " " + self.make + " " + self.model
+        return long_name.title()
+
+my_new_car = Car("audi", "a4", 2016)
+print(my_new_car.get_descriptive_name())
