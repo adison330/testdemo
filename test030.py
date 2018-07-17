8f5e9f0bb2d025ac2b66491533e01fea28985183
adison_han <[38833263@qq.com]>
2018-07-17 18:02:08 +0800

    修改能不能提交上

--- test030.py
+++ test030.py
@@ -1,9 +1,9 @@
 #! usr/bin/env python
 
 import requests
-url = "http://music.163.com/api/playlist/detail?id=402614161"
+url = "http://music.163.com/api/song/lyric?os=pc&id=411988938&lv=-1&kv=-1&tv=-1"
 req = requests.get(url)
 data = req.json()
 
-print(req)
+#print(req)
 print(data)
---
+++ test031.py
@@ -0,0 +1,5 @@
+#! usr/bin/env python
+
+import jieba
+seg_list = jieba.cut("我做的说唱从来不是为了混这个圈子", cut_all=True)
+print("Full Mode: " + "/".join(seg_list))
