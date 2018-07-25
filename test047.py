4c257e228f2e629f8559ce1b281cf8b46ccfcba2
adison_han <[38833263@qq.com]>
2018-07-25 16:07:07 +0800

    提交了

--- test047.py
+++ test047.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 from test046 import Die
+import pygal
 
 #创建一个D6
 die = Die()
@@ -11,7 +12,7 @@ for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+#print(results)
 
 # 分析结果
 frequencies = []
@@ -19,4 +20,15 @@ for value in range(1, die.num_sides+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
-print(frequencies)
+#print(frequencies)
+
+#对结果进行可视化
+hist = pygal.Bar()
+
+hist.title = "Results of rolling one D6 1000 times."
+hist.x_title = "结果"
+hist.y_title = "Frequency of Result"
+
+hist.add("D6", frequencies)
+hist.render_to_file("die_visual.svg")
+
---
+++ test050.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+import csv
+
+filename = "weather.csv"
+with open (filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    print(header_row)
